d750f835b1ea4a4108f6bd0227fe1d7ed97b9d1d
Juan Hamilton <hedwards507@gmail.com>
2023-01-06 23:36:45 -0500

    Add hover state to navbar icons

--- root/components/common/navbar.tsx
+++ root/components/common/navbar.tsx
@@ -2,16 +2,12 @@ import Stack, { StackProps } from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import React from 'react'
 import Link from './link'
-import Box from '@mui/material/Box'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import SvgIcon from '@mui/material/SvgIcon'
 
 const Navbar = ({ sx }: StackProps) => {
-  const navbarLinks = [
-    { imgSrc: '/icon-github.svg', alt: 'Github icon' },
-    { imgSrc: '/icon-frontend-mentor.svg', alt: 'Frontend Mentor icon' },
-    { imgSrc: '/icon-linkedin.svg', alt: 'LinkedIn icon' },
-    { imgSrc: '/icon-twitter.svg', alt: 'Twitter icon' },
-  ]
-
   return (
     <Stack
       component='nav'
@@ -25,12 +21,31 @@ const Navbar = ({ sx }: StackProps) => {
           adamkeyes
         </Typography>
       </Link>
-      <Stack direction='row' spacing={3} sx={{ mt: '2rem' }}>
-        {navbarLinks.map((link, index) => (
-          <Link href='#' key={index}>
-            <Box component='img' src={link.imgSrc} alt={link.alt} />
-          </Link>
-        ))}
+      <Stack
+        direction='row'
+        spacing={3}
+        sx={{
+          mt: '2rem',
+          '& > .MuiSvgIcon-root': {
+            cursor: 'pointer',
+            color: theme => theme.palette.common.white,
+            transition: theme =>
+              theme.transitions.create('color', {
+                easing: theme.transitions.easing.easeIn,
+                duration: theme.transitions.duration.shortest,
+              }),
+            ':hover': {
+              color: theme => theme.palette.primary.main,
+              background: 'none',
+            },
+          },
+        }}>
+        <GitHubIcon fontSize='large' />
+        <SvgIcon fontSize='large'>
+          <path d='M13.084.23a.751.751 0 0 0-.736.75v14.267a.75.75 0 1 0 1.5 0V.98a.75.75 0 0 0-.763-.75ZM24.44 4.504a.752.752 0 0 0-.284.064l-6.44 2.875a.752.752 0 0 0 0 1.37l6.44 2.884a.75.75 0 0 0 .612-1.369L19.861 8.13l4.907-2.191a.753.753 0 0 0 .38-.99.752.752 0 0 0-.708-.444ZM1.371 9.663a.752.752 0 0 0-.74.938C2.41 17.447 8.603 22.23 15.685 22.23a.75.75 0 1 0 0-1.501A14.053 14.053 0 0 1 2.083 10.225a.75.75 0 0 0-.712-.561v-.001Z' />
+        </SvgIcon>
+        <LinkedInIcon fontSize='large' />
+        <TwitterIcon fontSize='large' />
       </Stack>
     </Stack>
   )
